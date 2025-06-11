4dc9dccf49900c5dea94b0142e8eb9beaae732a8
Austregiselo Junior <austregiselosvj@gmail.com>
2025-06-10 21:25:57 -0300

    UnitTest: Subindo Dividir teste.

--- UnitTest.Test/Features/Calculadora/Services/CalculadoraTest.cs
+++ UnitTest.Test/Features/Calculadora/Services/CalculadoraTest.cs
@@ -18,5 +18,20 @@ namespace UnitTest.Test.Features.Calculadora.Services
             //Assert
             Assert.Equal(25, result);
         }
+
+        [Fact]
+        public void Verificar_Dividir_Retorna_Double()
+        {
+            //Arrage
+            double n1 = 8;
+            double n2 = 2;
+            CalculadoraService service = new CalculadoraService();
+
+            //Act
+            double result = service.Dividir(n1, n2);
+
+            //Assert
+            Assert.Equal(4, result);
+        }
     }
 }
