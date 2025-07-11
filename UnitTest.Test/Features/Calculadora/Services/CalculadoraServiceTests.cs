caefe5a7bd8a526df86094a291b5610b3a1c2af5
Austregiselo Junior <austregiselosvj@gmail.com>
2025-07-10 21:48:53 -0300

    UnitTest: Update Unit test

--- UnitTest.Test/Features/Calculadora/Services/CalculadoraServiceTests.cs
+++ UnitTest.Test/Features/Calculadora/Services/CalculadoraServiceTests.cs
@@ -1,15 +1,18 @@
 ﻿using Moq;
+using UnitTestwithXUnit.Features.Calculadora.Contracts;
 using UnitTestwithXUnit.Features.Calculadora.Services;
 
 namespace UnitTest.Test.Features.Calculadora.Services
 {
     public class CalculadoraServiceTests : IDisposable
     {
-        private MockRepository _mockRepository;
+        private readonly MockRepository _mockRepository;
+        private readonly Mock<ICalculadora> _mockICalculadora;
 
         public CalculadoraServiceTests()
         {
-            this._mockRepository = new MockRepository(MockBehavior.Strict);
+            _mockRepository = new MockRepository(MockBehavior.Strict);
+            _mockICalculadora = _mockRepository.Create<ICalculadora>();
         }
 
         private CalculadoraService CreateService()
@@ -21,26 +24,26 @@ namespace UnitTest.Test.Features.Calculadora.Services
         public void Calculadora_Dividir_DivisaoDosNumeros()
         {
             // Arrange
-            var service = this.CreateService();
+            var service = CreateService();
             double n1 = 8;
             double n2 = 4;
+            _mockICalculadora.Setup(x => x.Dividir(n1, n2)).Returns(2); // Mock o teste para retornar 2
 
             // Act
-            var result = service.Dividir(
-                n1,
-                n2);
+            var result = service.Dividir(n1, n2); // Chama o método Dividir do serviço
 
             // Assert
-            Assert.Equal(2, result);
+            Assert.Equal(2, result); // Verifica se o resultado é igual a 2
         }
 
         [Fact]
         public void Calculadora_Multiplicar_MultiplicacaoDosNumeros()
         {
             // Arrange
-            var service = this.CreateService();
+            var service = CreateService();
             double n1 = 2;
             double n2 = 4;
+            _mockICalculadora.Setup(x => x.Multiplicar(n1, n2)).Returns(8);
 
             // Act
             var result = service.Multiplicar(
@@ -58,6 +61,7 @@ namespace UnitTest.Test.Features.Calculadora.Services
             var service = this.CreateService();
             double n1 = 2;
             double n2 = 4;
+            _mockICalculadora.Setup(x => x.Somar(n1, n2)).Returns(6);
 
             // Act
             var result = service.Somar(
@@ -75,6 +79,7 @@ namespace UnitTest.Test.Features.Calculadora.Services
             var service = this.CreateService();
             double n1 = 6;
             double n2 = 4;
+            _mockICalculadora.Setup(x => x.Subtrair(n1, n2)).Returns(2);
 
             // Act
             var result = service.Subtrair(
@@ -85,9 +90,17 @@ namespace UnitTest.Test.Features.Calculadora.Services
             Assert.Equal(2, result);
         }
 
-        public void Dispose()
+        #region Dispose Methods
+        protected virtual void Dispose(bool disposing)
         {
             _mockRepository.VerifyAll();
         }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 }
